999668415166bbb0eb2d14c5bdb069f618f335e0
Taylor Thurlow <taylorthurlow8@gmail.com>
2017-06-17 14:14:16 -0700

    Updated staging deploy url

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'taylorjthurlow.com', user: 'taylor', roles: %w{app web db}
+server 'staging.taylorjthurlow.com', user: 'taylor', roles: %w{app web db}
 set :branch, 'staging'
 set :rails_env, 'staging'
 set :deploy_to, "/var/www/html/blog_staging"
