2bdffd8afcb241bec68306dc0e10b36f7639f280
Taylor Thurlow <taylorthurlow8@gmail.com>
2017-06-17 21:28:00 -0700

    Posts now using markdown

--- Gemfile
+++ Gemfile
@@ -30,7 +30,10 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'unicorn'
 
 # cool icons
-gem "font-awesome-rails"
+gem 'font-awesome-rails'
+
+# markdown
+gem 'redcarpet'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :contents, presence: true
 
+  def generate_markdown_html
+    renderer = Redcarpet::Render::HTML.new(render_options = {})
+    markdown = Redcarpet::Markdown.new(renderer, fenced_code_blocks: true)
+    return markdown.render(self.contents)
+  end
+
   def date_line
     line = 'posted ' + self.created_at.localtime.strftime('%b %e %Y, %l:%M %p')
     if self.created_at != self.updated_at
