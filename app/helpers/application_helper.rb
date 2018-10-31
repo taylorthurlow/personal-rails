3f7bec02025c152d4a763288b4c360428ad6ada8
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-10-31 13:52:58 -0700

    Add dynamic titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   include ActionView::Helpers::NumberHelper
 
+  def title(page_title)
+    content_for(:title) { "#{page_title} - Taylor Thurlow" }
+  end
+
   def current_user
     return Login.find(session[:login_id]) if session[:login_id]
     return nil
