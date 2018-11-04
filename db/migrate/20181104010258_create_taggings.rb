78be33f80314093e27a073d64a1b9d3525d7f452
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-11-03 23:25:02 -0700

    Don't create index for taggings, not supported on postgres

--- db/migrate/20181104010258_create_taggings.rb
+++ db/migrate/20181104010258_create_taggings.rb
@@ -3,7 +3,6 @@ class CreateTaggings < ActiveRecord::Migration[5.2]
     create_table :taggings do |t|
       t.belongs_to :post, foreign_key: true
       t.belongs_to :tag, foreign_key: true
-      t.add_index :tag_posts, [:tag_id, :post_id], unique: true
 
       t.timestamps
     end
