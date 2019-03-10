696c45f05b7d2c60ba462e89a23d2c31d2196ab3
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-03-10 12:35:13 -0700

    Add storage folder to capistrano linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :puma_init_active_record, true
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'storage')
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml' , 'public/files/resume.pdf')
 
 set :yarn_flags, '--production --silent --no-progress'
