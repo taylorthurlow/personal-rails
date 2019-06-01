2d95f0ddead480a73489d63cdb0f67b6112ddd71
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-06-01 12:27:20 -0700

    Set deploy ruby to 2.6.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "personal"
 set :user, "rails"
 set :puma_threads, [4, 16]
 set :puma_workers, 0
-set :rbenv_ruby, "2.5.1"
+set :rbenv_ruby, "2.6.3"
 
 # Don't change these unless you know what you're doing
 set :pty, true
